62ff553142c3dbbc044a7206c7ebd64a56ad64e7
UsmanAhmed1337 <UsmanAhmed1337@gmail.com>
2023-09-19 23:18:36 +0500

    Added flask

--- test_hello.py
+++ test_hello.py
@@ -2,15 +2,15 @@ from hello import app
 
 def test_add():
     with app.test_client() as client:
-        response = client.get('add/4/2')
+        response = client.get('/add/4/2')
         assert response.get_data(as_text=True) == '6'
 
 def test_sub():
     with app.test_client() as client:
-        response = client.get('sub/4/2')
+        response = client.get('/sub/4/2')
         assert response.get_data(as_text=True) == '2'
 
 def test_mul():
     with app.test_client() as client:
-        response = client.get('mul/4/2')
+        response = client.get('/mul/4/2')
         assert response.get_data(as_text=True) == '8'
