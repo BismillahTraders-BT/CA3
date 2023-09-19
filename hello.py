f7ea22979f86917d6ee801370fc7c43943a9c64d
waleedbinosama <waleedbinosama@gmail.com>
2023-09-20 01:02:45 +0500

    hel

--- hello.py
+++ hello.py
@@ -8,16 +8,16 @@ def hello():
 
 @app.route("/add/<int:a>/<int:b>")
 def add(a,b):
-    return (a+b)
+    return str(a+b)
 
 @app.route("/sub/<int:a>/<int:b>")
 def sub(a, b):
-    result = a - b
+    result = str(a - b)
     return result
 
 @app.route("/mul/<int:a>/<int:b>")
 def mul(a, b):
-    return a * b
+    return str(a * b)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
